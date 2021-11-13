eb9fb1d0f3383761374accd9a41e174faf53ba59
akshay <akshay.yamgar29@gmail.com>
2021-11-13 11:41:59 +0530

    Added: Test Case for Validate Firstname

--- src/main/java/com/bridgelabz/userregistrationjunit/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistrationjunit/UserRegistration.java
@@ -1,6 +1,24 @@
 package com.bridgelabz.userregistrationjunit;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * User Registration Junit Testing
+ *
+ * @author : Akshay
+ * @version : 1.8
+ * @since : 09/11/2021
+ *
+ */
 public class UserRegistration {
+    public boolean firstName(String firstName) {
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(firstName);
+        return matcher.matches();
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome To User Registration Problem Using Junit Testing.");
     }
--- src/test/java/com/bridgelabz/userregistrationjunit/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/userregistrationjunit/UserRegistrationTest.java
@@ -1,4 +1,25 @@
 package com.bridgelabz.userregistrationjunit;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class UserRegistrationTest {
+    UserRegistration userRegistration;
+//    @Before
+//    public void before() {
+//        userRegistration = new UserRegistration();
+//    }
+
+    @Test
+    public void givenFirstNameWhenProperShouldReturnTrue() {
+        boolean result = userRegistration.firstName("Akshay");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenFirstNameWhenNotProperShouldReturnFlase() {
+        boolean result = userRegistration.firstName("akshay");
+        Assert.assertEquals(false, result);
+    }
 }
